d235e5aefef0d015de96dbb67003da645f12f3ee
Phoenix James <mrthet2@hotmail.com>
2019-05-23 14:10:17 +0630

    intial

---
+++ database/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '1Gg61520!6',
+    database: 'tdi_data'
+});
+
+connection.connect(function(err) {
+  if (err) {
+      console.error('ERR connecting: ' + err.stack);
+  } else {
+      console.log('CONNECTED AS ID ' + connection.threadId);
+  }
+});
+
+module.exports = connection;
---
+++ routes/township.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var connection = require('../database/connection');
+
+var getAllTownship = function (req, res) {
+    const query = `SELECT * FROM township`;
+  
+    connection.query(query, function (err, result) {
+      if (err) {
+        console.error(err);
+        return res.status(400).send(err);
+      }
+  
+      return res.status(200).send(result);
+    });
+  };
+
+  var getTownshipBySRPCode = function (req, res) {
+    let p_code = req.params.srpcode;
+    const query = `SELECT * FROM township where SR_PCODE = '${p_code}'`;
+  
+    connection.query(query, function (err, result) {
+      if (err) {
+        console.error(err);
+        return res.status(400).send(err);
+      }
+  
+      return res.status(200).send(result);
+    });
+  };
+
+router.get('/', getAllTownship);
+router.get('/:srpcode', getTownshipBySRPCode);
